5bc0bea3de16b2d69a5dd167eca5f25128478a26
Denys N <denys@joomla-ua.org>
2019-01-13 01:21:31 +0200

    Add support namespases and fix small bugs

--- juarticlesave.php
+++ juarticlesave.php
@@ -6,21 +6,27 @@
  * @subpackage       plg_content_juarticlesave
  *
  * @author           Denys Nosov, denys@joomla-ua.org
- * @copyright        2018 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
+ * @copyright        2018-2019 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
  * @license          GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
-jimport('joomla.plugin.plugin');
+require_once __DIR__ . '/lib/emt/EMT.php';
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\CMSPlugin;
 
 /**
- * Example Content Plugin
+ * JUArticleSave Content Plugin
  *
  * @since  1.0
  */
-class PlgContentJUarticlesave extends JPlugin
+class PlgContentJUarticlesave extends CMSPlugin
 {
+	private $app;
+
 	/**
 	 * PlgContentJUarticlesave constructor.
 	 *
@@ -28,12 +34,14 @@ class PlgContentJUarticlesave extends JPlugin
 	 * @param $config
 	 *
 	 * @throws \Exception
+	 *
+	 * @since  1.0
 	 */
 	public function __construct(&$subject, $config)
 	{
 		parent::__construct($subject, $config);
 
-		$this->app = JFactory::getApplication();
+		$this->app = Factory::getApplication();
 	}
 
 	/**
@@ -47,17 +55,18 @@ class PlgContentJUarticlesave extends JPlugin
 	 */
 	public function onContentBeforeSave($context, $article, $isNew)
 	{
-		if($context != 'com_content.article')
+		if( $context !== 'com_content.article' )
 		{
 			return true;
 		}
 
-		if($this->params->def('fix_date', 0) == 1)
+		if( $this->params->def('fix_date', 0) == 1 )
 		{
 			date_default_timezone_set('UTC');
+
 			$datenow = date('Y-m-d H:i:s');
 
-			if($article->publish_up == '0000-00-00 00:00:00' || $article->publish_up == null || $article->publish_up == '')
+			if( $article->publish_up === '0000-00-00 00:00:00' || $article->publish_up === null || $article->publish_up == '' )
 			{
 				$article->publish_up = $datenow;
 				$article->created    = $datenow;
@@ -67,13 +76,13 @@ class PlgContentJUarticlesave extends JPlugin
 				$article->created = $article->publish_up;
 			}
 
-			$msg_publis_up = '<strong>' . JText::_('COM_CONTENT_FIELD_PUBLISH_UP_LABEL') . ':</strong>&nbsp;&nbsp;&nbsp;' . JHtml::date($article->publish_up, 'Y-m-d H:i:s') . '<br>';
-			$msg_created   = '<strong>' . JText::_('COM_CONTENT_FIELD_CREATED_LABEL') . '</strong>:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . JHtml::date($article->created, 'Y-m-d H:i:s');
+			$msg_publis_up = '<strong>' . Text::_('COM_CONTENT_FIELD_PUBLISH_UP_LABEL') . ':</strong>&nbsp;&nbsp;&nbsp;' . JHtml::date($article->publish_up, 'Y-m-d H:i:s') . '<br>';
+			$msg_created   = '<strong>' . Text::_('COM_CONTENT_FIELD_CREATED_LABEL') . '</strong>:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . JHtml::date($article->created, 'Y-m-d H:i:s');
 
 			$this->app->enqueueMessage($msg_publis_up . $msg_created, 'notice');
 		}
 
-		if($this->params->def('typo', 0) == 1)
+		if( $this->params->def('typo', 0) == 1 )
 		{
 			$article->title     = $this->_typo($article->title, 0);
 			$article->introtext = $this->_typo($article->introtext);
@@ -85,7 +94,9 @@ class PlgContentJUarticlesave extends JPlugin
 	}
 
 	/**
-	 * @param $text
+	 * @param     $text
+	 *
+	 * @param int $tags
 	 *
 	 * @return mixed|null|string|string[]
 	 *
@@ -93,21 +104,19 @@ class PlgContentJUarticlesave extends JPlugin
 	 */
 	public function _typo($text, $tags = 1)
 	{
-		require_once __DIR__ . '/lib/emt/EMT.php';
+		$typograf = new EMTypograph();
 
 		preg_match_all('!(\[socpost\].*?\[/socpost\])!si', $text, $pre);
 		$text = preg_replace('!\[socpost\].*?\[/socpost\]!si', '#pre#', $text);
 
-		$typograf = new EMTypograph();
-
 		$typograf->set_text($text);
-		$typograf->setup(array(
+		$typograf->setup([
 			'Text.paragraphs' => 'off',
 			'Text.breakline'  => 'off',
 			'OptAlign.all'    => 'off',
-		));
+		]);
 
-		if($tags == 0)
+		if( $tags == 0 )
 		{
 			$result = html_entity_decode(strip_tags($typograf->apply()));
 		}
@@ -116,9 +125,9 @@ class PlgContentJUarticlesave extends JPlugin
 			$result = $typograf->apply();
 			$result = str_replace('<p></p>', '', $result);
 
-			if(!empty($pre[ 0 ]))
+			if( !empty($pre[ 0 ]) )
 			{
-				foreach($pre[ 0 ] as $tag)
+				foreach( $pre[ 0 ] as $tag )
 				{
 					$result = preg_replace('!#pre#!', $tag, $result, 1);
 				}
